ce829080e2b9c112749498f3659d5cb4f7612a6e
Muhammad Hannan Hasan Ali <muhammadhannanhasanali@outlook.com>
2024-02-22 15:51:40 +0500

    User Interface Updated to resolve the error.

--- SSAPI/Repository/Interface/EmployeeManagement/IMstUser.cs
+++ SSAPI/Repository/Interface/EmployeeManagement/IMstUser.cs
@@ -2,11 +2,11 @@
 {
     public interface IMstUser
     {
-        Task<MstUser> GetUser(int id);
+        Task<MstUser> GetUser(Guid id);
         Task<List<MstUser>> GetUserList();
         Task<bool> AddUser(MstUser oRecord);
         Task<bool> UpdateUser(MstUser oRecord);
-        Task<bool> DeleteUser(int id);
+        Task<bool> DeleteUser(Guid id);
 
     }
 }
