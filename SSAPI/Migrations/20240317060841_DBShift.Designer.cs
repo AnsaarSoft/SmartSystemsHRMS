d6228d06b6dff4289e8257a4c51bf37200a58bc4
Muhammad Hannan Hasan Ali <muhammadhannanhasanali@outlook.com>
2024-03-24 15:16:44 +0500

    Login UI updated

--- SSAPI/Migrations/20240317060841_DBShift.Designer.cs
+++ SSAPI/Migrations/20240317060841_DBShift.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SSAPI.Context;
 
@@ -11,9 +12,10 @@ using SSAPI.Context;
 namespace SSAPI.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240317060841_DBShift")]
+    partial class DBShift
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
