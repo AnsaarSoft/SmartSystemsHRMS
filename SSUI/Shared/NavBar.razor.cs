186a87a5119c7d5ea7e75ae7b71e5ab4bce91bc3
Faisal <mfmjnj@gmail.com>
2024-03-31 12:32:27 +0500

    Logout button added

---
+++ SSUI/Shared/NavBar.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SSUI.Shared
+{
+    public partial class NavBar
+    {
+        #region Variable
+
+
+        #endregion
+
+        #region Dependency
+
+        [Inject] ILogger<MainLayout> logger { get; set; }
+        [Inject] NavigationManager navigation { get; set; }
+        [Inject] ProtectedLocalStorage storage { get; set; }
+        #endregion
+
+        #region Functions
+
+        async Task LogoutUser()
+        {
+            try
+            {
+                await storage.DeleteAsync("userinfo");
+                await storage.DeleteAsync("accesstoken");
+                navigation.NavigateTo("login", true);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, ex.Message);
+            }
+        }
+
+        #endregion
+    }
+}
